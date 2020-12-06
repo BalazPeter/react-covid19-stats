7df6c12d325e72ddb1a17932da8e3c1f668f60b1
Peter Baláž <peter.balaz.2@student.tuke.sk>
2020-12-06 22:40:43 +0100

    Removing snapshots

--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -1,27 +1,27 @@
-import Enzyme, { render, mockPropsForComponent } from 'enzyme';
+import Enzyme, { render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../../App';
 Enzyme.configure({ adapter: new Adapter() });
 
 
-describe('A series of tests', () => {
-    const originalConsoleError = console.error;
+// describe('A series of tests', () => {
+//     const originalConsoleError = console.error;
   
-    beforeEach(() => {
-      console.error = jest.fn((msg) => {
-        if (msg.includes('Warning: useLayoutEffect does nothing on the server')) {
-          return null;
-        } else {
-          originalConsoleError(msg);
-        }
-      });
-    });
+//     beforeEach(() => {
+//       console.error = jest.fn((msg) => {
+//         if (msg.includes('Warning: useLayoutEffect does nothing on the server')) {
+//           return null;
+//         } else {
+//           originalConsoleError(msg);
+//         }
+//       });
+//     });
   
-    afterEach(() => {
-      console.error = originalConsoleError;
-    });
-    it('App snapshot', () => {
-        const wrapper = render(<App />);
-        expect(wrapper).toMatchSnapshot();
-    });
-  });
+//     afterEach(() => {
+//       console.error = originalConsoleError;
+//     });
+//     it('App snapshot', () => {
+//         const wrapper = render(<App />);
+//         expect(wrapper).toMatchSnapshot();
+//     });
+//   });
